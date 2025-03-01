fe943ae670c9e080eef2003fc0259dfd11d8f2c5
Danylo <107757498+DanyloDiachenko@users.noreply.github.com>
2025-03-01 20:24:51 +0200

    _

--- components/common/MyKeys/index.tsx
+++ components/common/MyKeys/index.tsx
@@ -7,36 +7,54 @@ import { useEffect, useState } from "react";
 
 const MyKeys = () => {
     const t = useTranslations("Profile");
+
     const [keys, setKeys] = useState<{
-        trello: string;
+        trelloKey: string;
+        trelloToken: string;
         jira: string;
         shopify: string;
     }>({
-        trello: "",
+        trelloKey: "",
+        trelloToken: "",
         jira: "",
         shopify: "",
     });
-
-    useEffect(() => {
-        console.log(keys);
-    }, [keys]);
 
     return (
         <div className="w-full text-primary bg-secondary-light p-6 rounded-md mx-auto">
             <span className="font-semibold text-lg">{t("your-keys")}</span>
             <div className="mt-8 grid grid-cols-1 gap-4">
-                <LabelInput
-                    labelTitle="Trello"
-                    value={keys.trello}
-                    placeholder={t("trello-placeholder")}
-                    id="trello"
-                    onChange={(e) => {
-                        setKeys({
-                            ...keys,
-                            trello: e.target.value,
-                        });
-                    }}
-                />
+                <div>
+                    <div className="grid grid-cols-2 gap-4">
+                        <LabelInput
+                            labelTitle="Trello Key"
+                            value={keys.trelloKey}
+                            placeholder={t("trello-key-placeholder")}
+                            id="trello-key"
+                            onChange={(e) => {
+                                setKeys({
+                                    ...keys,
+                                    trelloKey: e.target.value,
+                                });
+                            }}
+                        />
+                        <LabelInput
+                            labelTitle="Trello Token"
+                            value={keys.trelloToken}
+                            placeholder={t("trello-token-placeholder")}
+                            id="trello-token"
+                            onChange={(e) => {
+                                setKeys({
+                                    ...keys,
+                                    trelloToken: e.target.value,
+                                });
+                            }}
+                        />
+                    </div>
+                    <p className="text-gray mt-2 text-sm">
+                        {t("trello-instruction")}
+                    </p>
+                </div>
                 <LabelInput
                     labelTitle="Jira"
                     value={keys.jira}
